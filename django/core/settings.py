03a0ccd9a572e406d6e4e4c15da088c99b7d7135
1mgroot <40841493+1mgroot@users.noreply.github.com>
2022-12-13 17:58:24 -0500

    change access token life time to 15 mins

--- django/core/settings.py
+++ django/core/settings.py
@@ -151,6 +151,6 @@ CORS_ALLOWED_ORIGINS = [
 AUTH_USER_MODEL = 'users.UserData'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
 }
